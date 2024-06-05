d5a344d6e0a23680216583af830051035990b292
pifflox <daiyanrafi1@gmail.com>
2024-06-05 13:21:46 +0600

    n

--- cypress/e2e/Test2.cy.js
+++ cypress/e2e/Test2.cy.js
@@ -15,7 +15,8 @@ describe('My Second Test Suite', function () {
 
             const textVeg = $el.find('h4.product-name').text()
             if (textVeg.includes('Cashews')) {
-                $el.find('button').click()
+                cy.wrap($el).find('button').click()
+
             }
         })
         cy.get('.cart-icon > img').click()
